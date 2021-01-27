e6500b79f3e809d1b8784c4068c65116464e8c65
Peter Ting <pting1995@gmail.com>
2021-01-26 18:16:51 -0800

    fixed how api writes and recieves new/old notes

--- server.js
+++ server.js
@@ -1,32 +1,54 @@
 // dependencies
-var express = require("express");
-var path = require("path");
-// const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // sets up express
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // sets up express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-// key=value&key2=value2 => {key:value, key2:value2}
 app.use(express.json());
 
 app.use(express.static("public"));
 
-const db = require("./db/db.json")
-
-// // recieves object
+// recieves object
 app.get("/api/notes", function (req, res) {
-    res.json(db);
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
 });
-// // sends object
-app.post("/api/notes", function (req, res) {
+// sends object
+app.post("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
 
-    db.push(req.body);
+        // old notes
+        const db = JSON.parse(data);
 
-    res.json(db);
+        const dbArray = [];
+
+        // new notes = req.body
+
+        db.push(req.body);
+
+        for (var i = 0; i < db.length; i++) {
+            const newNote = {
+                id: i + 1,
+                title: db[i].title,
+                note: db[i].note
+            };
+            dbArray.push(newNote);
+        }
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(dbArray), (err) => {
+            if (err) throw err;
+            res.json(req.body);
+        })
+    })
 })
+
 
 
 
